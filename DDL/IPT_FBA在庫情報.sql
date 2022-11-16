9b154ca41eebc52cc218665333651d83f7b717fe
lijinghui0529 <117079188+lijinghui0529@users.noreply.github.com>
2022-11-16 19:06:14 +0900

    11-16

---
+++ DDL/IPT_FBA在庫情報.sql
@@ -0,0 +1,11 @@
+create table public."IPT_FBAºß¸Ë¾ðÊó" (
+  "seller-sku" character varying(50)
+  , "fulfillment-channel-sku" character varying(50)
+  , asin character varying(50)
+  , "condition-type" character varying(50)
+  , "Warehouse-Condition-code" character varying(50)
+  , "Quantity Available" character varying(50)
+  , "Å¹ÊÞID" character varying(50)
+  , "ÅÐÏ¿Æü»þ" character varying(50)
+  , "¹¹¿·Æü»þ" character varying(50)
+);
